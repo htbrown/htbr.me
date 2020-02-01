b777bae935b9c624f240001de351446edbdcd3ad
Hayden Brown <nedyah.brown@gmail.com>
2020-02-01 13:00:47 +0000

    Fixed & problem

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    port: 4000,
-    discord: {
-        id: '671106705127636992',
-        secret: 'KWC_iH-edkzvSCxYrmLgkdNaQEa5JHnK',
-        authedIDs: ["517803652069589000"]
-    }
-}
